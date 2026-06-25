f5a671ea1d2557645c655a3257673b514fd6b465
rebeccallewelyn <techbot_1010@outlook.com>
2026-06-25 18:17:25 +1000

    Add GET/POST request_access route

--- project/routes.py
+++ project/routes.py
@@ -1,16 +1,26 @@
-from flask import Blueprint, render_template
-from project.model import CollectionItem
+from flask import Blueprint, render_template, redirect, flash, url_for, session
+from project.model import CollectionItem, AccessRequest
 from project.forms import AccessRequestForm
 #from project.auth import login_required
 
 items_bp = Blueprint('items_bp', __name__)
 
 #@login_required -- confirm decorator name with Carrie
-@items_bp.route('/items/<int:item_id>/request', methods=['GET'])
+@items_bp.route('/items/<int:item_id>/request', methods=['GET', 'POST'])
 
 def request_access(item_id):
     item = CollectionItem.get_by_id(item_id)
-    form = AccessRequestForm()
     if item is None:
         return render_template("error.html", error_code=404)
+
+    form = AccessRequestForm()
+
+    if form.validate_on_submit():
+        user_id = session['user_id']
+        requestReason = form.requestReason.data
+        supportingDocuments = form.supportingDocuments.data
+        AccessRequest.create(user_id, item_id, requestReason, supportingDocuments)
+        flash('Your access request has been submitted', 'success')
+        return redirect(url_for('items_bp.request_access', item_id=item_id))
+
     return render_template("request_form.html", item=item, form=form)
