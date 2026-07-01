64aec4f5eba3b119abf189d584b81d008848ca9a
carriegale2710 <carriegale2710@gmail.com>
2026-07-02 05:16:24 +1000

    update status to pending upon new access request

--- project/views.py
+++ project/views.py
@@ -116,6 +116,7 @@ def request_access(item_id):
         supportingDocuments = form.supportingDocuments.data
 
         AccessRequest.create(userID, item_id, requestReason, supportingDocuments)
+        CollectionItem.update_status(item_id, 4)  # Update item status to 'Pending' when a new access request is submitted
         flash('Your access request has been submitted', 'success')
         return redirect(url_for('views.request_access', item_id=item_id))
 
